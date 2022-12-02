0da362f93e9847407a24143c55fb9ce32d6e422f
ncaillin <nugentcaillin@gmail.com>
2022-12-02 18:00:43 +1100

    4.4

--- blog-list/tests/list_helper.test.js
+++ blog-list/tests/list_helper.test.js
@@ -5,3 +5,60 @@ test('dummy returns 1', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithManyBlogs = [
+        {
+            _id: '63872c6cc82815464b23307a',
+            title: 'Flea rearing for beginners',
+            author: 'Kiwi',
+            url: 'https://www.notafakeurl.com/blogs/1.html',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: '63872e3d83e2ba87161809e4',
+            title: 'How to cook Bacon',
+            author: 'Apple',
+            url:'https://www.notafakeurl.com/blogs/2.html',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '63872e6c60ea5fd58adbb549',
+            title: 'Twiddling your thumbs',
+            author: 'Pomegranate',
+            url: 'https://www.notafakeurl.com/blogs/3.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithManyBlogs)
+        expect(result).toBe(20)
+    })
+})
--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+
+const totalLikes = (blogs) => {
+    
+    return blogs.reduce(
+        (total, currentBlog) => total + currentBlog.likes,
+        0
+    )
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
